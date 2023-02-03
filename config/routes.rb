4b36b6d7c37e751862d7c28a7853acf6037e3f46
Keith Ma <95742118+imkeithyeet@users.noreply.github.com>
2023-02-02 23:37:41 -0800

    sessions

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,8 @@ Rails.application.routes.draw do
   resources :users
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
+  get "/me", to: "sessions#show"
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 end
