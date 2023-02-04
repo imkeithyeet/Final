9a2dacde89c8ee9a2b1ae17d26f94b3b87ecb03a
Keith Ma <95742118+imkeithyeet@users.noreply.github.com>
2023-02-03 16:53:08 -0800

    backend

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,2 +1,14 @@
 class PhotosController < ApplicationController
+    def index 
+        photo = Photo.all 
+        render json: photo, status: :ok
+    end
+    def show
+        photo = find_photo
+        render json: photo
+    end
+    private
+    def find_photo
+        Photo.find_by(id: params[:id])
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,22 @@ class UsersController < ApplicationController
         user = User.all 
         render json: user, status: :ok
     end
+    def show
+        user = find_user
+        render json: user
+    end
+    def create
+        user = User.create!(user_params)
+        session[:user_id] = user.id
+        render json: user, status: :created
+    end
+    private 
+    def user_params
+        params.permit(:username,:email, :password, :password_confirmation)
+    end
+    private
+    def find_user
+        User.find_by(id: params[:id])
+    end
 end
+
