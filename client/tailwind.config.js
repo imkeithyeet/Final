6395f64d1dfe2e225b3816bf205735abc635a3d3
Keith Ma <95742118+imkeithyeet@users.noreply.github.com>
2023-02-05 23:41:28 -0800

    stuff

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js}",
-    "./components/**/*.{js}"
+    './pages/**/*.{html,js}',
   ],
   theme: {
     extend: {},
