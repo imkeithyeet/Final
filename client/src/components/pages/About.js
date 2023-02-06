23182c0b8c948e342ca5b6f0f0ff3bc1d5b2caa5
Keith Ma <95742118+imkeithyeet@users.noreply.github.com>
2023-02-05 21:29:59 -0800

    line

--- client/src/components/pages/About.js
+++ client/src/components/pages/About.js
@@ -12,7 +12,7 @@ function About() {
                     <img className="Img"src={"https://i.pinimg.com/736x/bf/30/b9/bf30b9d3a4be30122a45cc8162ed1124.jpg"} alt="family" />
                 <p1 className="p1">Relationships are at the core of everything we do. </p1>
                 <h className="h">We want anyone looking love.</h>
-                <h className="h1">to be able to find it</h>
+                <h1 className="H">to be able to find it</h1>
             </div>
         </wrapper>
     )
