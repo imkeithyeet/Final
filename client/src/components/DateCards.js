75401d70476edadae460c4cdbab52af5d680ab88
Keith Ma <95742118+imkeithyeet@users.noreply.github.com>
2023-02-08 03:26:02 -0800

    stuff

--- client/src/components/DateCards.js
+++ client/src/components/DateCards.js
@@ -35,40 +35,33 @@ const Property = () => {
                     <img src={user.image_url} alt="image" />
                 </div>
 
-                <div className="property-content">
-                    <h1>{user.first_name}</h1>
+                <div className="name">
+                    <h1>{user.first_name} {user.last_name}</h1>           
                     <div className="mark">
                       <i class="fa-solid fa-location-dot"></i>
                        <p1>{user.gender}</p1>
                     </div>
-                    <h3>{user.age}</h3>
+                    <p1>{user.age}</p1>
                 </div>
 
                 <div className="property-content">
-                    <h3>{user.occupation}</h3>
                     <div className="mark">
                       <i class="fa-solid fa-location-dot"></i>
                        <h3>{user.location}</h3>
                     </div>
                     <h3>{user.bio}</h3>
                 </div>
-
-
+                <div>
+                    <p>{user.horoscope}</p>
+                    </div>
             </div>
           </div>
         </div>
             )
         })
-    }
-
-        
+    }     
     </div>
-
-    <div className="more-property">
-    </div>
-
     </section>
-    
     </Fragment>
   )
 }
