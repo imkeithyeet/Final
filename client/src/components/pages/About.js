c862c1a5e50c111a709be5052079ad26d8ae560d
Keith Ma <95742118+imkeithyeet@users.noreply.github.com>
2023-02-15 23:57:58 -0800

    fonts

--- client/src/components/pages/About.js
+++ client/src/components/pages/About.js
@@ -18,7 +18,7 @@ function About() {
 
                 <h5 className="H5">At the heart of Us.</h5>
                     <img className="Img"src={"https://i.pinimg.com/736x/bf/30/b9/bf30b9d3a4be30122a45cc8162ed1124.jpg"} alt="family" />
-                <p className="p1">Relationships are at the core of everything we do. </p>
+                <p className="p1">Relationships are at the core of everything we do. Our mission <br></br>is to create a safe and inclusive space where people from all walks of <br></br>life can find meaningful connections with others. </p>
                 <h1 className="h">We want anyone looking love</h1>
                 <h1 className="H">to be able to find it</h1>
             </div>
